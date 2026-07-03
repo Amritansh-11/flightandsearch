e8b2c40c4e4930826039f0fe30b181b6cb83997e
Amritansh-11 <amritanshs41@gmail.com>
2026-07-03 16:49:33 +0530

    setup basic env

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express= require("express");
+const {PORT }=require('./config/serverConfig');
+
+const setupAndstartserver =async ()=>{
+    const app=express();
+    
+    app.listen(PORT ,()=>{
+      console.log(`Server started at ${PORT}`)
+      
+    }
+
+)
+}
+setupAndstartserver();
