1e7d0349c47191aafd6a9548f0b569c2bf9d0d6c
Amritansh-11 <amritanshs41@gmail.com>
2026-07-04 21:36:37 +0530

    setup database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express= require("express");
+
 const {PORT }=require('./config/serverConfig');
 
 const setupAndstartserver =async ()=>{
     const app=express();
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+
+
     
     app.listen(PORT ,()=>{
       console.log(`Server started at ${PORT}`)
